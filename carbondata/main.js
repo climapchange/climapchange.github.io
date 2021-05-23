52e5a28268f52fa4e9f0be3034a944e79685edcf
geo6371 <jens-weise@hotmail.de>
2021-05-24 00:18:49 +0200

    Funktionierende Daten-Abfrage nach Eingabe ArrayNr

--- carbondata/main.js
+++ carbondata/main.js
@@ -41,16 +41,27 @@ console.log('Länder: ', countries)
 //Hier muss die Länder-Zahl (Array-Nr.) des Landes eingetragen werden (0-231)
 //console.log('Die Anzahl der Poly-Länder beträgt: ', COUNTRY[0].features.length)
 //console.log(COUNTRY[0].features)
-let polyNr = 231
+
+let polyNr = 50
 console.log(polyNr)
 
+//Zur Überprüfung, ob ein Land nicht gleich benannt ist. Dies ist nicht der Fall.
+/*
+for (let polyNr = 0; polyNr < COUNTRY[0].features.length; polyNr++){
+    console.log(COUNTRY[0].features[polyNr].properties.name_long);
+  }
+*/
+
 //Auf Polygon-Daten zugreifen. Die Zahl hinter features[ZAHL] bestimmt das Land
-let polyName = COUNTRY[0].features[polyNr].properties.formal_en;
+let polyName = COUNTRY[0].features[polyNr].properties.name_long;
 console.log('polyName: ',polyName)
 
 //um an die Zahl zu kommen nach 'data' muss ich die Länge herausfinden und minus 1 rechnen.
 let lastYear = CODATA[0].country[polyName].data.length - 1
-console.log('Die Zahl des letzten gelisteten Jahres ist: ', lastYear)
+console.log('Die Array-Nr. des letzten gelisteten Jahres ist: ',lastYear)
+
+let year = CODATA[0].country[polyName].data[lastYear].year
+console.log('Die Daten beziehen sich auf das Jahr: ',year)
 
 //Automatisch anhand des Poly-Namens nach den CO2 Daten suchen lassen
 let coSelect = CODATA[0].country[polyName].data[lastYear].co2
@@ -58,15 +69,15 @@ console.log('Die CO2-Daten des gewähten Landes betragen: ',coSelect)
 
 //So werden die ISO-Daten abgerufen. Land muss variabel
 let iso = CODATA[0].country[polyName].iso_code
-console.log('Der ISO-Code aus den CO2Daten lautet:', iso)
+console.log('Der ISO-Code aus den CO2Daten lautet: ',iso)
 
 //Auf Polygon-Daten zugreifen. Die Zahl hinter features[ZAHL] bestimmt das Land
 let polyIso = COUNTRY[0].features[polyNr].properties.iso_a3;
-console.log('Der ISO-Code aus den Poly-Daten lautet: ', polyIso)
+console.log('Der ISO-Code aus den Poly-Daten lautet: ',polyIso)
 
 
-//Das klappt schon ganz gut! Allerdings scheinen die Namen bei COUNTRY nicht immer denen bei CODATA zu entsprechen. Beispiel polyNr = 1. Hier ist dann der ISO-Code nötig, was das ganze komplzierter macht, weil es innerhalb von CODATA tief genested ist und z.B über .find() gesucht werden muss
 
+//Wahrscheinlich muss das alles in eine Funktion, wie beim AWS-Beispiel. Also getDirection wird dann in Windrichtungspopup gespeichert und das wird im Popup erst gecalled.
 
 
 
