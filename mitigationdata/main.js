4ea9d21ef7afaea3bc498fc825d12987e355f13e
asboehle <Ann-Sophie.Boehle@student.uibk.ac.at>
2021-05-27 11:02:30 +0200

    Kommentare

--- mitigationdata/main.js
+++ mitigationdata/main.js
@@ -1,8 +1,8 @@
+//Kartenhintergründe definieren
 let baselayers = {
     standard: L.tileLayer.provider("OpenStreetMap.DE"),
     topo: L.tileLayer.provider("OpenTopoMap"),
 };
-
 
 const map = L.map("map", {
     fullscreenControl: true,
@@ -13,11 +13,13 @@ const map = L.map("map", {
     ]
 });
 
+//Kartenhintergründe + Overlays zur Layer-Kontrolle
 let layerControl = L.control.layers({
     "Standard": baselayers.standard,
     "Relief": baselayers.topo,
 }).addTo(map);
 
+//Mini Map
 var miniMap = new L.Control.MiniMap(L.tileLayer.provider("OpenStreetMap.DE"), {
     toggleDisplay: true,
     minimized: false,
