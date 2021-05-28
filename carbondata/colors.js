fbc72c320efc1d9bb8d4ddf43679ebc3701fb48e
geo6371 <jens-weise@hotmail.de>
2021-05-28 19:50:55 +0200

    Farbrampe funktioniert!!!

--- carbondata/colors.js
+++ carbondata/colors.js
@@ -1,12 +1,13 @@
 function getColor(feature) {
     let polyName = feature.properties.name_long;
-    console.log(polyName);
-    let coSelect = (CODATA[0].country[polyName].data[CODATA[0].country[polyName].data.length - 1].co2);
-    console.log(coSelect);
+    //console.log(polyName);
+    let coData = (CODATA[0].country[polyName].data[CODATA[0].country[polyName].data.length - 1].co2);
+    //console.log(coSelect);
     //let lastYear = CODATA[0].country[polyName].data.length - 1;
     //let coSelect = CODATA[0].country[polyName].data[lastYear].co2;
-    let coData = 500; 
-    return coData > 1000 ? '#800026' :
+    //let coData = 500; 
+    return coData > 99998 ? '#808080' :
+        coData > 1000 ? '#800026' :
         coData > 500 ? '#BD0026' :
         coData > 200 ? '#E31A1C' :
         coData > 100 ? '#FC4E2A' :
