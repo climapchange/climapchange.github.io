56cec4ff0fa931fb4a4a7a8a05f4d70e76408da8
SarahRasi <sarah.rasi@student.uibk.ac.at>
2021-06-11 17:20:22 +0200

    set tree marker

--- bestpractice/main.js
+++ bestpractice/main.js
@@ -56,9 +56,9 @@ overlays.UmweltKlima.addTo(map);
 
 //Icons
 var UmweltIcon = L.icon({
-    iconUrl: 'tree.png',
+    iconUrl: 'icons/tree.png',
 
-    iconSize:     [38, 95], // size of the icon
+    iconSize:     [38, 38], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
